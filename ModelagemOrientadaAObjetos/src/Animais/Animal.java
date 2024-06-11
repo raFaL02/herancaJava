05122c28f1f9daa75c8accf516ce92e9eea179e4
Rafael de Lima <rafalimabcl@gmail.com>
2024-06-10 22:46:18 -0300

    feat: add Animal Class

---
+++ ModelagemOrientadaAObjetos/src/Animais/Animal.java
@@ -0,0 +1,17 @@
+package Animais;
+
+public class Animal {
+    protected String name;
+    protected String color;
+    protected int height;
+    protected double weight;
+    protected String spiritState;
+
+    protected void comer() {}
+
+    protected void dormir() {}
+
+    protected void soar() {
+        System.out.println(" ");
+    }
+}
